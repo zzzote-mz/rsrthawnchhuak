fa1e0c308f43dcc082ebeb95377771de75bc9327
RSR-Downloader <79363075+RSR-Downloader@users.noreply.github.com>
2021-03-09 22:11:27 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -6,7 +6,7 @@ from bot import JunctionBot
 @JunctionBot.on_message(filters.command("start") & filters.incoming & filters.private)
 async def _(c: JunctionBot, m: Message):
     await m.reply_text(
-        "Hi {} I am a Junction Bot. I can forward messages from one chat to multiple chats",
+        "Hi {} Ka hming chu ZigZag ania. Automatic a chat forward thei Bot ka ni e,min siamtu hi @rsrmusic ani e",
         parse_mode="markdown",
         reply_to_message_id=m.message_id
     )
